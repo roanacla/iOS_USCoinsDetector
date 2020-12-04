c521392ccf239fa113578926a96cc5eb279aa77d
roanacla <roanacla@me.com>
2020-12-03 16:50:52 -0800

    Start iOS empty project

---
+++ iOS_USCoinsDetector/iOS_USCoinsDetectorTests/iOS_USCoinsDetectorTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_USCoinsDetectorTests.swift
+//  iOS_USCoinsDetectorTests
+//
+//  Created by Roger Navarro on 12/3/20.
+//
+
+import XCTest
+@testable import iOS_USCoinsDetector
+
+class iOS_USCoinsDetectorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
